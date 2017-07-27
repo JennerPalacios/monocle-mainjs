725badd11932afac303c3a8b1350ae6e2a0d7668
JennerPalacios <japp_17@hotmail.com>
2017-07-26 23:58:08 -0700

    Add files via upload

---
+++ discord.js
@@ -0,0 +1,49 @@
+var discord= new Array[ MessageEmbed {
+    message: 
+     Message {
+       channel: [Object],
+       id: '339685183680806912',
+       type: 'DEFAULT',
+       content: '',
+       author: [Object],
+       member: null,
+       pinned: false,
+       tts: false,
+       nonce: null,
+       system: false,
+       embeds: [Circular],
+       attachments: Collection {},
+       createdTimestamp: 1501057658835,
+       editedTimestamp: null,
+       reactions: Collection {},
+       mentions: [Object],
+       webhookID: '314565219999547392',
+       hit: null,
+       _edits: [] },
+	   
+	   
+    type: 'rich',
+    title: '♀ **lvl:** 30 | IV: 64.4% (10/14/5)',
+    description: '**Moves**: Scratch/Cross Chop\n**Location**: Parkwood, Shoreline\n**Until**: 01:52:58 (25m 20s)\n**→ Trainer level 30+! ←**',
+    url: 'http://maps.google.com/maps?q=47.7347177417,-122.3475947222',
+    color: undefined,
+    fields: [],
+    createdTimestamp: undefined,
+    thumbnail: 
+     MessageEmbedThumbnail {
+       embed: [Circular],
+       url: 'https://raw.githubusercontent.com/kvangent/PokeAlarm/master/icons/216.png',
+       proxyURL: 'https://images-ext-2.discordapp.net/external/b1YaneT-iC5djxqtpLx4Qe3MDKNjIhvtUwQDNH4uxXA/https/raw.githubusercontent.com/kvangent/PokeAlarm/master/icons/216.png',
+       height: 128,
+       width: 128 },
+    image: 
+     MessageEmbedImage {
+       embed: [Circular],
+       url: 'https://maps.googleapis.com/maps/api/staticmap?center=47.7347177417,-122.347594722&markers=color:red%7C47.7347177417,-122.347594722&maptype=roadmap&size=250x125&zoom=15&key=AIzaSyD-ZeHg9qi3j2TDI2kdOmfEdYvXnexH5WQ',
+       proxyURL: 'https://images-ext-2.discordapp.net/external/l8G5FvidcOF1aC2C_S7QZdW-OrTr_68MS1sRm7lHfKI/%3Fcenter%3D47.7347177417%2C-122.347594722%26markers%3Dcolor%3Ared%257C47.7347177417%2C-122.347594722%26maptype%3Droadmap%26size%3D250x125%26zoom%3D15%26key%3DAIzaSyD-ZeHg9qi3j2TDI2kdOmfEdYvXnexH5WQ/https/maps.googleapis.com/maps/api/staticmap',
+       height: 0,
+       width: 0 },
+    video: null,
+    author: null,
+    provider: null,
+    footer: null } ];
